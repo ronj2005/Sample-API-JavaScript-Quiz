b01e6b6cf06168a7e06b417ad2c4d17c82662d45
Aaron Jones <ronj2005@gmail.com>
2021-09-23 00:52:07 -0400

    CSS and Script Question update

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,7 +1,7 @@
 var startBtn = document.querySelector(".startBtn");
 var timeElement= document.querySelector(".timer-count");
 var timeOut = document.querySelector(".time-out");
-
+var btnBox = document.querySelector(".box1")
 
 
 startBtn.addEventListener("click", function (event) {
@@ -17,6 +17,7 @@ startBtn.addEventListener("click", function (event) {
         startBtn.setAttribute("visible", "hidden");
         console.log("working");
         startBtn.style.display = "none"
+        btnBox.style.display = "none"
     }
 
     var secLeft = 60;
@@ -41,38 +42,72 @@ startBtn.addEventListener("click", function (event) {
         }, 1000);
 
         var questions = [];
-
+// questions array
 var questions = [
     {
-    Question: "What is 15+8?",
-    choices: [3, 23, 33, 43],
-    answer: 23,
+    Question: "Which of the following is a server-side Java Script object?",
+    choices: ["Function", "File", "File Upload", "Date"],
+    answer: "Date",
     }, 
     {
-    Question: "What is 99 * 3?",
-    choices: [13, 223, 433, 553],
-    answer: 233,
+    Question: "What is the alternate name for Java script?",
+    choices: ["LimeScript", "Both a and b", "ECMScript", "ECMAScript"],
+    answer: "ECMAScript",
     },
     {
-    Question: "WHat is Middle Name",
-    choices: ["Jack", "Job", "Jones", "Luna"],
-    answer: "Luna",
+    Question: "To insert a JavaScript into an HTML page, which tag is used?",
+    choices: ["< script=’java’>", "< javascript>", "< script>", "<js>"],
+    answer: "<script>",
     },
     {
-    Question: "What is US capital?",
-    choices: [13, 223, 433, 553],
-    answer: 233,
-    }
+    Question: "Which of the below is used in Java script to insert special characters?",
+    choices: ["&", "/", "-", "%"],
+    answer: "&",
+    },
+    {
+    Question: "Which attribute needs to be changed to make elements invisible?",
+    choices: ["visibility", "visible", "invisibility", "invisible"],
+    answer: "visibility",
+    },
+    {
+    Question: "What java wrapper type is created when a JavaScript object is sent to Java?",
+    choices: ["ScriptObject", "JavaObject", "jobject", "JSObject"],
+    answer: "JSObject",
+    },
+    {
+    Question: "Which is the correct way to write a JavaScript array?",
+    choices: ["&", "/", "-", "%"],
+    answer: "&",
+    },
+    {
+    Question: "Which of the following is correct to write “Hello World” on the web page?",
+    choices: ["&", "/", "-", "%"],
+    answer: "&",
+    },
+    {
+    Question: "Which of the following is correct to write “Hello World” on the web page?",
+    choices: ["&", "/", "-", "%"],
+    answer: "&",
+    },
+    {
+    Question: "Which of the following is the tainted property of a window object in Java Script?",
+    choices: ["&", "/", "-", "%"],
+    answer: "&",
+    },
 ];
 
 
 var indexCurrentQ = 0;
+var indexCurrentA = 0;
 
 //increamenting index value to get the next question
-var nextQuestion = document.querySelector(".nextQuestion");
-nextQuestion.addEventListener("click", function(event) {
+var nextQuestionSet = document.querySelector(".nextQuestion");
+var nextAnswerSet = document.querySelector(".answerBtn");
+nextQuestionSet.addEventListener("click", function(event) {
     event.preventDefault();
+varQuestionBox = nextAnswerSet + nextQuestionSet; 
     indexCurrentQ++;
+    indexCurrentA++;
     getQuestion();
 })
 
